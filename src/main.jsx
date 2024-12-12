194f50b3984acad88a6d8ea310d4649cd24403f3
sydorovay <sydorovay@gmail.com>
2024-12-12 22:10:54 +0100

    add globals syles
    reset, color, body, a, button.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/globals.css'
 import App from './components/App/App.jsx'
 
 createRoot(document.getElementById('root')).render(
