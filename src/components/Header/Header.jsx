954054f1898bc97e734d7bcd77a4e82aadab9791
sydorovay <sydorovay@gmail.com>
2024-12-12 23:37:26 +0100

    welkome page + header
    uodate + css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,19 +2,17 @@ import css from "./Header.module.css";
 
 export default function Header() {
   return (
-    <div div>
-      <header class="header">
-        <div class="container">
-          <a href="#" class="logo">
-            Tracker of Water
+    <header className={css.header}>
+      <div className={css.container}>
+        <a href="#" className={css.logo}>
+          Tracker of Water
+        </a>
+        <nav className={css.nav}>
+          <a href="/signin" className={css.navLink}>
+            Sign in
           </a>
-          <nav class="nav">
-            <a href="/signin" class="nav-link">
-              Sign in
-            </a>
-          </nav>
-        </div>
-      </header>
-    </div>
+        </nav>
+      </div>
+    </header>
   );
 }
