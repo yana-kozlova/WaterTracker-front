6afa2676e57a5875868c13fe046508bd7ff6bfa0
RomanFS76 <korvy85@gmail.com>
2024-12-13 23:10:38 +0200

    add Provider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './styles/globals.css'
-import App from './components/App/App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./styles/globals.css";
+import App from "./components/App/App.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
