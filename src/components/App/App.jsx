aa3ae1c300c20fbdac98ab235d354afb2316f56b
Yana Kozlova <jasenkozlova@gmail.com>
2024-12-14 21:26:56 +0000

    DripLoader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from 'react'
 import Layout from "../Layout/Layout.jsx";
+import DripLoader from "../DripLoader/DripLoader.jsx";
 
 const WelcomePage = lazy(() => import('../../pages/WelcomePage/WelcomePage.jsx'));
 const MainPage = lazy(() => import('../../pages/MainPage/MainPage.jsx'));
