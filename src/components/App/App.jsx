b3d42932b65d5664fe51620da17eb813d4d64a7d
sydorovay <sydorovay@gmail.com>
2024-12-14 22:20:31 +0100

    add loader
    Використання кастомного хука для завантаження.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,16 @@
+import { useLoading } from '../DripLoader/UseLoading.jsx'; 
 import WelcomePage from "../../pages/WelcomePage/WelcomePage.jsx";
+import DripLoader from "../DripLoader/DripLoader.jsx";
 
 function App() {
+  const isLoading = useLoading(3000); // Встановлюємо тривалість завантаження
+
   return (
     <>
-      <WelcomePage />
+      {isLoading && <DripLoader />}
+      {!isLoading && <WelcomePage />}
     </>
   );
 }
+
 export default App;
