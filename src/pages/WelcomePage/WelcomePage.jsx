5df1a20fcacb3daecb249582909e623f520074d8
sydorovay <sydorovay@gmail.com>
2024-12-14 11:27:31 +0100

    css
    update css + userLogo

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,4 +1,3 @@
-
 import css from "./WelcomePage.module.css"
 import Header from "../../components/Header/Header"
 import TryTrackerButton from "../../components/Buttons/TryTrackerButton/TryTrackerButton"
@@ -6,52 +5,50 @@ import TryTrackerButton from "../../components/Buttons/TryTrackerButton/TryTrack
 export default function WelcomePage() {
   return (
     <>
-      <Header/>
+      <Header />
       <main className={css.main}>
         <div className={css.container}>
-  <section className="hero">
-    <div className="hero-content">
-      <h1 className="hero-title">Water consumption tracker</h1>
-      <p className="hero-subtitle">
-        Record daily water intake and track
-      </p>
-      <ul className="benefits">
-        <li className="benefit">
-          <span className="icon">&#128197;</span>
-          <span className="text">Habit drive</span>
-        </li>
-        <li className="benefit">
-          <span className="icon">&#128202;</span>
-          <span className="text">View statistics</span>
-        </li>
-        <li className="benefit">
-          <span className="icon">&#9881;</span>
-          <span className="text">Personal rate setting</span>
-        </li>
-      </ul>
-
-      <TryTrackerButton/>
-    </div>
-
-    <aside className="why-drink-water">
-      <h2>Why drink water</h2>
-      <ul>
-        <li>Supply of nutrients to all organs</li>
-        <li>Providing oxygen to the lungs</li>
-        <li>Maintaining the work of the heart</li>
-        <li>Release of processed substances</li>
-        <li>Ensuring the stability of the internal environment</li>
-        <li>Maintaining within the normal temperature</li>
-        <li>Maintaining an immune system capable of resisting disease</li>
-      </ul>
-    </aside>
-  </section>
-</div>
+          <section className={css.hero}>
+            <div className={css.heroContent}>
+              <h1 className={css.heroTitle}>Water consumption tracker</h1>
+              <p className={css.heroSubtitle}>
+                Record daily water intake and track
+              </p>
+              <ul className={css.benefits}>
+                <li className={css.benefit}>
+                  <span className={css.icon}>&#128197;</span>
+                  <span className={css.text}>Habit drive</span>
+                </li>
+                <li className={css.benefit}>
+                  <span className={css.icon}>&#128202;</span>
+                  <span className={css.text}>View statistics</span>
+                </li>
+                <li className={css.benefit}>
+                  <span className={css.icon}>&#9881;</span>
+                  <span className={css.text}>Personal rate setting</span>
+                </li>
+              </ul>
+              <TryTrackerButton />
+            </div>
+            <aside className={css.wyDrinkWater}>
+              <h2>Why drink water</h2>
+              <ul>
+                <li>Supply of nutrients to all organs</li>
+                <li>Providing oxygen to the lungs</li>
+                <li>Maintaining the work of the heart</li>
+                <li>Release of processed substances</li>
+                <li>Ensuring the stability of the internal environment</li>
+                <li>Maintaining within the normal temperature</li>
+                <li>Maintaining an immune system capable of resisting disease</li>
+              </ul>
+            </aside>
+          </section>
+        </div>
 
       </main>
 
-      <footer className="footer">
-        <div className="container">
+      <footer className={css.footer}>
+        <div className={css.container}>
           <p>&copy; 2024 Tracker of Water. All rights reserved.</p>
         </div>
       </footer>
