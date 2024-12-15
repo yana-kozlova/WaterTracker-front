2a75376492cc2e3ac05db61d6ea54fd281766d93
RomanFS76 <korvy85@gmail.com>
2024-12-15 18:06:47 +0200

    add auth redux selectors

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,5 +1,5 @@
 import { createSlice, isAnyOf } from "@reduxjs/toolkit";
-import { register, login, logout } from "./operations";
+import { register, login, logout,refreshUser } from "./operations";
 
 const initialState = {
   user: {},
