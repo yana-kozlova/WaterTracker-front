d9e4b0b6d596503f5d3e3f52999c5eb24d28d3cd
sydorovay <sydorovay@gmail.com>
2024-12-15 10:03:55 +0100

    Update App.jsx
    fix: add Suspense import and Driploader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,19 @@
 import { Route, Routes } from "react-router-dom";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import Layout from "../Layout/Layout.jsx";
+import DripLoader from "../DripLoader/DripLoader.jsx";  
 
 const MainPage = lazy(() => import("../../pages/MainPage/MainPage.jsx"));
 const SigninPage = lazy(() => import("../../pages/SigninPage/SigninPage.jsx"));
 const SignupPage = lazy(() => import("../../pages/SignupPage/SignupPage.jsx"));
 const NotFoundPage = lazy(() => import("../../pages/NotFoundPage.jsx"));
-const WelcomePage = lazy(
-  () => import("../../pages/WelcomePage/WelcomePage.jsx")
+const WelcomePage = lazy(() => import("../../pages/WelcomePage/WelcomePage.jsx")
 );
 
 export default function App() {
   return (
     <>
+      <Suspense fallback={<DripLoader />}>
         <Routes>
           <Route path="/" element={<Layout />} />
           <Route path="/welcome" element={<WelcomePage />} />
@@ -21,6 +22,7 @@ export default function App() {
           <Route path="/signin" element={<SigninPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
+        </Suspense>
     </>
   );
 }
