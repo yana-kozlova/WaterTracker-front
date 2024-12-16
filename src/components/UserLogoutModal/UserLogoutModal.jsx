2938ccb54b8e21b5e2a70008fea25ed5423450bc
SVelikiy <108265367+SVelikiy@users.noreply.github.com>
2024-12-16 22:59:42 +0200

    add-logout-modal

---
+++ src/components/UserLogoutModal/UserLogoutModal.jsx
@@ -0,0 +1,25 @@
+import { logout } from "../../redux/auth/operations";
+import Button from "../Buttons/Button/Button";
+import css from "./UserLogoutModal.module.css";
+import { useDispatch } from "react-redux";
+
+const LogoutModal = () => {
+  const dispatch = useDispatch();
+  return (
+    <div className={css.container}>
+      <p className={css.title}>Log out</p>
+      <p className={css.text}>Do you really want to leave?</p>
+      <div className={css.buttonContainer}>
+        <Button
+          type="button"
+          name="Log out"
+          className={css.buttonLogout}
+          onClick={() => dispatch(logout())}
+        />
+        <Button type="button" name="Cancel" className={css.buttonCancel} />
+      </div>
+    </div>
+  );
+};
+
+export default LogoutModal;
