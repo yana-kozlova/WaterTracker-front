0dd58668a94d81ec6db3986bc06a1fc108f00f22
Qvantt <65224747+Qvantt@users.noreply.github.com>
2024-12-19 15:41:38 +0200

    DailyNorma almost done

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import DailyNormaModal from "./DailyNormaModal";
-import "./DailyNorma.css";
+import DailyNormaModal from "../DailyNormaModal/DailyNormaModal";
+import styles from "./DailyNorma.module.css";
 
 const DailyNorma = () => {
   const [dailyNorm, setDailyNorm] = useState(2.0); // Початкове значення: 2.0 літри
@@ -20,14 +20,17 @@ const DailyNorma = () => {
   };
 
   return (
-    <div className="daily-norma">
-      <p>Запланована денна норма води: {dailyNorm} л</p>
-      <button className="edit-button" onClick={handleEditClick}>
-        Edit
-      </button>
-      {isModalOpen && (
-        <DailyNormaModal currentNorm={dailyNorm} onSave={updateDailyNorm} />
-      )}
+    <div className={styles["daily-norma"]}>
+      <p className={styles["daily-norma-p"]}>My daily norma </p>
+      <div className={styles["edit-container"]}>
+        <p className={styles["daily-norma-l"]}>{dailyNorm} L</p>
+        <button className={styles["edit-button"]} onClick={handleEditClick}>
+          Edit
+        </button>
+        {isModalOpen && (
+          <DailyNormaModal currentNorm={dailyNorm} onSave={updateDailyNorm} />
+        )}
+      </div>
     </div>
   );
 };
