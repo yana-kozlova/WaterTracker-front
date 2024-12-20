a73336da24cc19ae21298851478fb5ba34ee9c94
sydorovay <sydorovay@gmail.com>
2024-12-20 12:50:29 +0100

    update css

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,5 +1,6 @@
 import css from './WelcomePage.module.css'; // Імпортуємо стиль
 import MainContainer from "./components/Container/MainContainer";
+
 import Footer from "../../components/Footer/Footer";
 
 export default function WelcomePage() {
