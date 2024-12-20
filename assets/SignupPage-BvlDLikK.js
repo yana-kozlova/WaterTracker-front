fa322c46cb82badc077ba6aa4f492b56e3507ed8
yana-kozlova <jasenkozlova@gmail.com>
2024-12-20 23:10:01 +0000

    Deploying to gh-pages from @ yana-kozlova/WaterTracker-front@6769d848a68306f2e955977301ffa3922ff8f864 🚀

---
+++ assets/SignupPage-BvlDLikK.js
@@ -0,0 +1,2 @@
+import{j as r}from"./index-DvQpiDEO.js";import{A as t}from"./Auth-DmzVo5ZY.js";const o=()=>r.jsx(r.Fragment,{children:r.jsx(t,{})});export{o as default};
+//# sourceMappingURL=SignupPage-BvlDLikK.js.map
