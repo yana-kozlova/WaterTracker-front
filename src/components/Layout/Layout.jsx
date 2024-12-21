baf371c155f5eae5036236e5d49153706c217f4f
Alexandr <alexandr.kopyletz@gmail.com>
2024-12-22 00:23:53 +0200

    22.12

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,16 +3,16 @@ import { Outlet } from "react-router-dom";
 // import DaysGeneralStats from '../DaysGeneralStats/DaysGeneralStats.jsx';
 import DripLoader from '../DripLoader/DripLoader.jsx';
 import Header from "../Header/Header";
-import BaseModal from "../BaseModal/BaseModal.jsx";
+// import BaseModal from "../BaseModal/BaseModal.jsx";
 // import WaterRatioPanel from "../WaterRatioPanel/WaterRatioPanel.jsx"
-import TodayListModal from "../TodayListModal/TodayListModal.jsx"
+// import TodayListModal from "../TodayListModal/TodayListModal.jsx"
 
 export default function Layout() {
   return (
     <>
-    <BaseModal isOpen={true}> /*додаємо модалку*/
-        <TodayListModal/> /*вкладаємо свій компонент*/
-      </BaseModal>
+    {/* <BaseModal isOpen={true}> /*додаємо модалку*/ */}
+        {/* <TodayListModal/> /*вкладаємо свій компонент*/ */}
+      {/* </BaseModal> */}
       <Header />
       {/* <DaysGeneralStats consumedWater={600} dailyNorm={1500} portions={5} selectedDate={new Date()}/> */}
       <Suspense fallback={<DripLoader />}>
