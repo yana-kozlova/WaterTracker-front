2f75d8da2c2bc0c2d376f0da2373b4b7a35070f9
Nastya <nastya.syasyafi@gmail.com>
2024-12-21 21:17:48 +0200

    add1

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,12 +1,49 @@
 import { Link } from "react-router-dom";
+import React from "react";
 
-export default function NotFoundPage() {
+const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 Not found page </h1>
-      <p>
-        Please use this link <Link to="/">back home</Link>
-      </p>
+    <div style={styles.container}>
+      <div style={styles.bottle}></div>
+      <button
+        style={styles.button}
+        onClick={() => (window.location.href = "/")}
+      >
+        Go to Homepage
+      </button>
     </div>
   );
-}
+};
+
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    backgroundColor: "#f7f9fc",
+    fontFamily: "Arial, sans-serif",
+    position: "relative",
+  },
+  bottle: {
+    width: "200px",
+    height: "400px",
+    backgroundImage: <img src="../components/404background/Background Main Page.png" alt="" />, 
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "contain",
+    backgroundPosition: "center",
+  },
+  button: {
+    backgroundColor: "#007bff",
+    color: "#fff",
+    border: "none",
+    padding: "10px 20px",
+    borderRadius: "5px",
+    cursor: "pointer",
+    fontSize: "16px",
+    marginTop: "20px",
+  },
+};
+
+export default NotFoundPage;
