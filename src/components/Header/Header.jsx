c8e076b8df264a8e98710b8a76c8795567a6c28d
Yana Kozlova <jasenkozlova@gmail.com>
2024-12-22 20:00:38 +0200

    review - navigation page - unfinished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import css from "./Header.module.css";
 import Logo from "../Logo/Logo";
 import { Link } from "react-router-dom";
-import Navigation from "../Navigation/Navigation";
+import Navigation from "./Navigation/Navigation";
 import Container from "../Container/Container";
 
 export default function Header() {
   return (
-    <header className={css.header}>
+    <header>
       <Container className={css.containerRow}>
         <Link to="/welcome">
           <Logo />
