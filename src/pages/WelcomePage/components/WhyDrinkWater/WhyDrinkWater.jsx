c3e2e8da17d3f7a178d61b85221dd46a8432561d
Yana Kozlova <jasenkozlova@gmail.com>
2024-12-22 16:17:15 +0200

    review - welcome page (unfihished)

--- src/pages/WelcomePage/components/WhyDrinkWater/WhyDrinkWater.jsx
+++ src/pages/WelcomePage/components/WhyDrinkWater/WhyDrinkWater.jsx
@@ -1,22 +1,27 @@
 import css from './WhyDrinkWater.module.css'
 
+const items = [
+  "Supply of nutrients to all organs",
+  "Providing oxygen to the lungs",
+  "Maintaining the work of the heart",
+  "Release of processed substances",
+  "Ensuring the stability of the internal environment",
+  "Maintaining within the normal temperature",
+  "Maintaining an immune system capable of resisting disease",
+];
+
+
 export default function WhyDrinkWater() {
   return (
-    <div className={css.container}>
-       <aside className={css.wyDrinkWater}>
-          <h2>Why drink water</h2>
-          <ul>
-            <li>Supply of nutrients to all organs</li>
-            <li>Providing oxygen to the lungs</li>
-            <li>Maintaining the work of the heart</li>
-            <li>Release of processed substances</li>
-            <li>Ensuring the stability of the internal environment</li>
-            <li>Maintaining within the normal temperature</li>
-            <li>
-              Maintaining an immune system capable of resisting disease
-            </li>
-          </ul>
-          </aside>
-    </div>
-  );
+      <aside className={css.whyDrinkWater}>
+        <h2>Why drink water</h2>
+        <div>
+          {items.map((item, index) => (
+            <div key={index} className={css.whyItem}>
+              <div className={css.whyDot}></div>
+              <div className={css.whyTitle}>{item}</div>
+            </div>
+          ))}
+        </div>
+      </aside>);
 }
