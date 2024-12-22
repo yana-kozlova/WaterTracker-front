26165c7d7f249c1cdfb9207dd8f782fc0d8bf676
Yana Kozlova <jasenkozlova@gmail.com>
2024-12-22 14:02:58 +0200

    review - auth

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,4 +1,4 @@
-import { SignInForm } from "../SignUp/SignUp.jsx";
+import { SignInForm } from "../AuthForm/AuthForm.jsx";
 import css from "./Auth.module.css";
 import BottleImg from "./BottleImg.jsx";
 
---
+++ src/components/AuthForm/const.jsx
@@ -0,0 +1,41 @@
+export const EyeIcon = ({ color = "#2F2F2F", size = 24, ...props }) => (
+  <svg
+    width={size}
+    height={size}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.574-3.007-9.964-7.178Z"
+      stroke={color}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+      stroke={color}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export const EyeSlashIcon = ({ color = "#2F2F2F", size = 24, ...props }) => (
+  <svg
+    width={size}
+    height={size}
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12c1.292 4.338 5.31 7.5 10.066 7.5.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65A3 3 0 1 0 9.88 9.879m4.242 4.242L9.881 9.88"
+      stroke={color}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
