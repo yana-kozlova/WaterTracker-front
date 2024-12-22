fcf2cbe0e0d14521acf1650460fe516346badf00
yana-kozlova <jasenkozlova@gmail.com>
2024-12-22 19:33:55 +0000

    Deploying to gh-pages from @ yana-kozlova/WaterTracker-front@399d8fa9ff954a410c4c3623b6759c51ddf63d46 🚀

---
+++ assets/SignupPage-DzziWJSi.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-DCV9ZmQV.js";import{A as r}from"./Auth-BfPiac04.js";const a=()=>t.jsx(r,{});export{a as default};
+//# sourceMappingURL=SignupPage-DzziWJSi.js.map
--- assets/SignupPage-RnWkbV2x.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./index-BypZQes-.js";import{A as r}from"./Auth-DoZNEZjd.js";const a=()=>t.jsx(r,{});export{a as default};
-//# sourceMappingURL=SignupPage-RnWkbV2x.js.map
