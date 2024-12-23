86bbc863ec94789a017d11e2cea66aacbaebefc6
Yana Kozlova <jasenkozlova@gmail.com>
2024-12-23 16:48:53 +0200

    home

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import MonthStatsTable from '../../components/MonthStatsTable/MonthStatsTable.jsx'
+import DailyNorma from '../../components/DailyNorma/DailyNorma.jsx';
 import TodayWaterList from '../../components/TodayWaterList/TodayWaterList.jsx';
 
 import css from './MainPage.module.css'
@@ -6,10 +7,18 @@ import css from './MainPage.module.css'
 export default function MainPage() {
   return (
     <>
-      <div className={css.waterContainer}>
-        <TodayWaterList/>
-        <MonthStatsTable />
-      </div>;
+      <div className={css.background}></div>
+      <div className={css.mainContainer}>
+        <div className={css.bottle}>
+          <div className={css.dailyNorma}>
+            <DailyNorma />
+          </div>
+        </div>
+        <div className={css.waterContainer}>
+          <TodayWaterList />
+          <MonthStatsTable />
+        </div>
+      </div>
     </>
   )
 }
