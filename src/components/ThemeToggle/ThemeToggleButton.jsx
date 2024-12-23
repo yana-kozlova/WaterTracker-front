e14c78b0cb831dfed0e70c3491599adf8ad4539e
sydorovay <sydorovay@gmail.com>
2024-12-23 09:53:11 +0100

    changed toogle

--- src/components/ThemeToggle/ThemeToggleButton.jsx
+++ src/components/ThemeToggle/ThemeToggleButton.jsx
@@ -1,14 +1,22 @@
-import { useTheme } from "../../context/ThemeContext";
-import lightModeIcon from "../../assets/moon.svg";
-import darkModeIcon from "../../assets/sun.svg";
+import { useTheme } from "../../context/ThemeContext"; // шлях до вашого контексту
+import styles from "./ThemeToggleButton.module.css";
 
 const ThemeToggleButton = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <button onClick={toggleTheme} className="theme-toggle-btn">
-      <img src={theme === "light" ? lightModeIcon : darkModeIcon} alt="Toggle Theme" />
-    </button>
+    <div
+      className={`${styles.slider} ${
+        theme === "dark" ? styles["slider--dark"] : styles["slider--light"]
+      }`}
+      onClick={toggleTheme}
+    >
+      <span className={styles.thumb}>
+        <span className={styles.knob}>
+          <span className={styles["central-knob"]}></span>
+        </span>
+      </span>
+    </div>
   );
 };
 
