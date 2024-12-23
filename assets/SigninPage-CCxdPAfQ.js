838fecfb2ab12d1f3ec7221b284c5cc6a219f27e
yana-kozlova <jasenkozlova@gmail.com>
2024-12-23 14:01:37 +0000

    Deploying to gh-pages from @ yana-kozlova/WaterTracker-front@e0ebc9f006270fd2b28ab62d36fb9a62e0173ed2 🚀

--- assets/SigninPage-CCxdPAfQ.js
+++
@@ -1,2 +0,0 @@
-import{j as t}from"./index-kQuUep8D.js";import{A as r}from"./Auth-CNf6DCQB.js";const a=()=>t.jsx(r,{});export{a as default};
-//# sourceMappingURL=SigninPage-CCxdPAfQ.js.map
---
+++ assets/SigninPage-C_2BbWsx.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-D61wkD46.js";import{A as r}from"./Auth-DWII_h1K.js";const a=()=>t.jsx(r,{});export{a as default};
+//# sourceMappingURL=SigninPage-C_2BbWsx.js.map
