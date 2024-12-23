9186d150dadb161c3f4ba624af917381cf5d8c3e
yana-kozlova <jasenkozlova@gmail.com>
2024-12-23 20:38:10 +0000

    Deploying to gh-pages from @ yana-kozlova/WaterTracker-front@2fd322820dfa924217cd98d745ffa5194e290e64 🚀

--- assets/NotFoundPage-BLEI3GCP.js
+++
@@ -1,2 +0,0 @@
-import{j as e,L as s}from"./index-C_-8b6_u.js";function i(){return e.jsxs("div",{children:[e.jsx("h1",{children:"404 Not found page "}),e.jsxs("p",{children:["Please use this link ",e.jsx(s,{to:"/",children:"back home"})]})]})}export{i as default};
-//# sourceMappingURL=NotFoundPage-BLEI3GCP.js.map
---
+++ assets/SignupPage-xvOw6nM8.js
@@ -0,0 +1,2 @@
+import{j as t}from"./index-7wSk4upP.js";import{A as r}from"./Auth-Vf175yZz.js";const a=()=>t.jsx(r,{});export{a as default};
+//# sourceMappingURL=SignupPage-xvOw6nM8.js.map
