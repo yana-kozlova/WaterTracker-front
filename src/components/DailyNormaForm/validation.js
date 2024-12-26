6ed639afeaec04991956d8364b8567be295282c8
Yana Kozlova <jasenkozlova@gmail.com>
2024-12-26 02:29:22 +0200

    small fix validation

--- src/components/DailyNormaForm/validation.js
+++ src/components/DailyNormaForm/validation.js
@@ -2,7 +2,7 @@ import * as Yup from 'yup';
 
 export const validationSchema = Yup.object({
   userDailyNorma: Yup.number()
-    .min(0.1, "Daily norm must be at least 0.001 liters")
+    .min(0.001, "Daily norm must be at least 0.001 liters")
     .max(15, "Daily norm can't be more than 15 liters")
     .required("Please enter your daily water intake"),
 });
